c15ea842ccaca98105d114a8c81b2c0ed782316c
pulkit agarwal <pulkitagarwal877@gmail.com>
2021-05-18 23:10:44 +0530

    shared preferences!!

--- app/src/main/java/com/example/android/counterapp/MainActivity.java
+++ app/src/main/java/com/example/android/counterapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.android.counterapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 import com.example.android.counterapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String COUNT_VALUE = "countValue";
     private int qty=0;
     private ActivityMainBinding b;
 
@@ -20,7 +22,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(b.getRoot());
 
         setupEventHandlers();
+
+        if (savedInstanceState != null) {
+            qty = savedInstanceState.getInt(COUNT_VALUE);
+        } else {
+            //Get data from sharedPreference
+            qty = getPreferences(MODE_PRIVATE)
+                    .getInt(COUNT_VALUE, 0);
+
+        }
+        b.qty.setText(qty + "");
     }
+
+
 
     private void setupEventHandlers() {
         b.incButton.setOnClickListener(new View.OnClickListener() {
@@ -45,4 +59,18 @@ public class MainActivity extends AppCompatActivity {
     public void incQty() {
         b.qty.setText(""+ ++qty);
     }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(COUNT_VALUE, qty);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        getPreferences(MODE_PRIVATE).edit().putInt(COUNT_VALUE, qty).apply();
+    }
 }
+
+
